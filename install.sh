49c79856450ea639232bd13f3ccb2d2cd9a6287d
shoumoji <kosyu79@gmail.com>
2020-10-24 02:56:56 +0900

    インデントを調整

--- install.sh
+++ install.sh
@@ -2,17 +2,14 @@
 
 cd $HOME/dotfiles
 
-if [ $? -ne 0 ]; then
-    echo "not found: $DOTPATH" >&2
-fi
 
 # 移動できたらリンクを実行する
 for f in `find .??*`
-  do
-    # .gitは無視
-    if [ $f = .git* ]; then
-      continue
-    fi
+do
+  # .gitは無視
+  if [ $f = .git* ]; then
+    continue
+  fi
 
-    ln -snfv "$DOTPATH/$f" "$HOME/$f"
-  done
+  ln -snfv "$DOTPATH/$f" "$HOME/$f"
+done
