fdf53eea0900b539247be5ee9a9b27e577fd043c
ayumin <kosyu79@gmail.com>
2020-11-25 12:57:18 +0900

    GOのPATHを追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
 alias vi='nvim'
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
+
+# set PATH
+set -U fish_user_paths $HOME/go/bin $fish_user_paths
+
+# set GOPATH
+set GOPATH $HOME/go $GOPATH
